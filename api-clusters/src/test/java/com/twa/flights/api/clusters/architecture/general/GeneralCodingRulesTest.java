cfb4381ca2e6921555ca5d41caa63f87a3ba69a9
asacco <sacco.andres@gmail.com>
2021-09-29 00:32:32 -0300

    final version

--- api-clusters/src/test/java/com/twa/flights/api/clusters/architecture/general/GeneralCodingRulesTest.java
+++ api-clusters/src/test/java/com/twa/flights/api/clusters/architecture/general/GeneralCodingRulesTest.java
@@ -7,7 +7,7 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
 @AnalyzeClasses(packages = ArchitectureConstants.DEFAULT_PACKAGE, importOptions = ImportOption.DoNotIncludeTests.class)
 class GeneralCodingRulesTest {
